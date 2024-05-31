f93e371692807cb40e521f20dd3711a55bc60a68
张殿伟 <dianwei.zhang@ghoststudio.net>
2024-05-31 17:54:22 +0800

    [tank]06

--- vue-project/src/tank/TankLogic.vue
+++ vue-project/src/tank/TankLogic.vue
@@ -1,19 +1,30 @@
 <script setup>
 import { useSeek, useTres, useTresContext } from '@tresjs/core'
-import { shallowRef, watch } from 'vue'
+import { shallowRef, watch, defineProps } from 'vue'
 import { useFBX } from '@tresjs/cientos'
 import { PerspectiveCamera, Vector3 } from 'three'
 
 const { camera, cameras, scene, setCameraActive, registerCamera } = useTresContext()
 const { seek, seekByName, seekAll, seekAllByName } = useSeek()
 
-useFBX("tank/main.fbx").then((obj) => {
-    scene.value.add(obj);
-    obj.position.set(-6.5, 0, 6.5)
+var b_init = false;
+
+const props = defineProps(['ResStoreObj'])
+watch(props, () => {
+    if (!b_init) {
+        init();
+        b_init = true;
+    }
 })
+
+function init() {
+    var main_fbx = props.ResStoreObj.main;
+    scene.value.add(main_fbx)
+    main_fbx.position.set(-7.5, 0, 7.5)
+}
+
 </script>
 
 <template>
-    <!-- <primitive :object="rootRef" :position="[-6.5, 0, 0]" /> -->
-    <!-- <TresObject3D></TresObject3D> -->
+
 </template>
--- vue-project/src/tank/TankMain.vue
+++ vue-project/src/tank/TankMain.vue
@@ -1,19 +1,41 @@
 <script setup>
 
 import { OrbitControls } from '@tresjs/cientos'
+import { useFBX } from '@tresjs/cientos'
 import TankLogic from "./TankLogic.vue"
+import { ref, shallowRef } from 'vue';
+
+const ResStoreNames = [
+    "tank/main.fbx",
+]
+const ResStoreObj = shallowRef(null)
+useFBX(ResStoreNames).then((objs) => {
+    const store = {}
+    for (let i = 0; i < objs.length; ++i) {
+        const obj = objs[i];
+        const name = ResStoreNames[i];
+        store[name] = obj;
+
+        var shortName = name.split('/')
+        shortName = shortName[shortName.length - 1]
+        shortName = shortName.split('.')
+        shortName = shortName[0]
+        store[shortName] = obj;
+    }
+    ResStoreObj.value = store;
+});
 
 </script>
 
 <template>
     <TresCanvas clear-color="#FDF5E6" window-size>
-        <TresPerspectiveCamera :position="[0, 17, 7]" :rotation="[-1.17, 0, 0]"></TresPerspectiveCamera>
+        <TresPerspectiveCamera :position="[0, 17, 7.5]" :rotation="[-1.19, 0, 0]"></TresPerspectiveCamera>
         <TresAmbientLight :intensity="1" />
 
         <TresAxesHelper></TresAxesHelper>
         <!-- <TresGridHelper></TresGridHelper> -->
 
-        <TankLogic></TankLogic>
+        <TankLogic :ResStoreObj="ResStoreObj"></TankLogic>
 
         <!-- <OrbitControls /> -->
     </TresCanvas>

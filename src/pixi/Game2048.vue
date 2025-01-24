85d1e5e7a41947876e22e39356758ed6caf567ec
张殿伟 <dianwei.zhang@ghoststudio.net>
2025-01-24 20:41:51 +0800

    [2048]01

--- src/pixi/Game2048.vue
+++ src/pixi/Game2048.vue
@@ -1,12 +1,13 @@
 <script setup>
 
 import { ref, shallowRef, watch, onMounted, onUnmounted } from 'vue';
-// import { Shader, Geometry, Point, Mesh, Circle, Graphics, Application, Assets, Container, Sprite } from 'pixi.js';
+import { Shader, Geometry, Point, Mesh, Circle, Graphics, Application, Assets, Container, Sprite } from 'pixi.js';
 import { OrbitControls } from '@tresjs/cientos'
 
 const pixiRef = shallowRef(null);
 const threeRef = shallowRef(null);
 var g = {};
+window.g = g;
 
 async function initGame() {
     let rope = new Sprite(g.assets.rope1);
@@ -17,24 +18,27 @@ async function initGame() {
 }
 
 function update() {
-    // g.rope.position.set(g.mousePos.x, g.mousePos.y);
+    if (g.mouse) {
+        if (g.mouse.buttons === 1) {
+            g.rope.position.set(g.mouse.global.x, g.mouse.global.y);
+        }
+    }
 }
 
 function onpointerdown(e) {
-    g.mousePos = e.global;
+    g.mouse = e;
 }
 
 function onpointermove(e) {
-    g.mousePos = e.global;
+    g.mouse = e;
 }
 
 function onpointerup(e) {
-    g.mousePos = e.global;
+    g.mouse = e;
 }
 
 onMounted(async () => {
-    return;
-    g.app = new Application({ background: '#eeeeee', resizeTo: pixiRef.value });
+    g.app = new Application({ resizeTo: pixiRef.value, transparent: true, backgroundAlpha: 0.0 });
     pixiRef.value.appendChild(g.app.view);
     g.center = { x: g.app.screen.width / 2, y: g.app.screen.height / 2 };
 
@@ -43,7 +47,6 @@ onMounted(async () => {
     g.app.stage.on("pointerdown", onpointerdown);
     g.app.stage.on("pointermove", onpointermove);
     g.app.stage.on("pointerup", onpointerup);
-    g.mousePos = { x: g.center.x, y: g.center.y };
 
     Assets.addBundle('assets', {
         "rope1": "img/rope1.png",
@@ -56,36 +59,29 @@ onMounted(async () => {
 })
 
 onUnmounted(() => {
-    // g.app.destroy(true, true);
+    g.app.destroy(true, true);
 })
 
 </script>
 
 <template>
-    <TresCanvas ref="threeRef" clear-color="#95e1d3" style="width: 200px; height: 200px;">
-        <TresPerspectiveCamera :position="[0, 20, 0]" :rotation="[0, 0, 0]"></TresPerspectiveCamera>
-        <TresAmbientLight :intensity="1" />
 
-        <TresAxesHelper></TresAxesHelper>
-        <TresGridHelper></TresGridHelper>
+    <div style="position: relative; width: 100%; height: 100vh;">
 
-        <!-- <OrbitControls /> -->
-    </TresCanvas>
-    <!-- <div style="position: relative; width: 100%; height: 100vh;"> -->
+        <div style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;">
+            <TresCanvas ref="threeRef" clear-color="#e0eee8" window-size>
+                <TresPerspectiveCamera :position="[0, 20, 0]" :rotation="[0, 0, 0]"></TresPerspectiveCamera>
+                <TresAmbientLight :intensity="1" />
 
-    <!-- <TresCanvas ref="threeRef" clear-color="#95e1d3"
-            style="position: absolute; top: 0; left: 0; width: 50%; height: 20%;">
-            <TresPerspectiveCamera :position="[0, 20, 0]" :rotation="[0, 0, 0]"></TresPerspectiveCamera>
-            <TresAmbientLight :intensity="1" />
+                <TresAxesHelper></TresAxesHelper>
+                <TresGridHelper></TresGridHelper>
 
-            <TresAxesHelper></TresAxesHelper>
-            <TresGridHelper></TresGridHelper>
+                <OrbitControls />
+            </TresCanvas>
+        </div>
 
-            <OrbitControls />
-        </TresCanvas> -->
+        <div ref="pixiRef" style=" position: absolute; top: 0; left: 0; width: 100%; height: 100%; ">
+        </div>
 
-    <!-- <div ref="pixiRef"
-            style="background-color: bisque; position: absolute; top: 0; left: 0; width: 100%; height: 20%;"></div> -->
-
-    <!-- </div> -->
+    </div>
 </template>
